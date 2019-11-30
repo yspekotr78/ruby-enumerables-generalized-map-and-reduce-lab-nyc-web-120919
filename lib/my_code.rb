01ba43d13c0ea17a6ec8d0e9f89aaef2c84794e0
Yury Spektor <yspektor78@gmail.com>
2019-11-30 20:51:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
 # Your Code Here
 def map(source_array)
   yield source_array
-  new_source = source_array.map { |x| -x }
-  new_source
+#  new_source = source_array.map { |x| -x }
+ # new_source
 end
 
 map(source_array){ |x| -x }
