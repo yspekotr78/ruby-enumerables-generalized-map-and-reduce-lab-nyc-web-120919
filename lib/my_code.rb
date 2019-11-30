00c1624d5ac793cdf6ef2411c991ea89fc2ca4a8
Yury Spektor <yspektor78@gmail.com>
2019-11-30 21:06:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,17 +1,21 @@
 # Your Code Here
 def map(array)
-  array.map { |element| element*-1 }
-end
-
-def map(array)
-  array
-end
-
-def map(source_array)
-  index = 0 
-  source_array.each do |number|
-    source_array[index] = yield(number)
-    index += 1 
+  p array
+  new_arr = []
+  i = 0
+  while i < array.length
+    new_arr.push(yield(array[i]))
+    i += 1
   end
-  source_array
-end 
+  new_arr
+end
+
+
+#def map(source_array)
+#  index = 0 
+#  source_array.each do |number|
+#   source_array[index] = yield(number)
+#    index += 1 
+#  end
+#  source_array
+#end 
