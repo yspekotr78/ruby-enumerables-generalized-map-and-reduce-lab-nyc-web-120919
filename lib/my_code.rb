c9d3b89e591df6a20c3631d12c19ddcf877050ae
Yury Spektor <yspektor78@gmail.com>
2019-11-30 21:02:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,11 @@ def map(array)
   array
 end
 
+def map(source_array)
+  index = 0 
+  source_array.each do |number|
+    source_array[index] = yield(number)
+    index += 1 
+  end
+  source_array
+end 
